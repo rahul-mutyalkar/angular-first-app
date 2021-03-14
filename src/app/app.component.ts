1dd12c3ac93e96e35ed30df9fcf7c4725ee430be
Rahul M <rahulmutyalkar@gmail.com>
2021-03-14 18:59:22 +0530

    first commit for learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstApp';
+  age = 10;
+  userJSON = {
+    name: "Sandeep Yadav",
+    mobile: "8483856421"
+  }
+  constructor() {
+    console.log('App Page started')
+  }
+
+  clickMe() {
+    console.log('you clicked me !!');
+  }
+
+  increment() {
+    this.age = this.age + 1;
+  }
+
+  decrement() {
+    if (this.age > 0) {
+      this.age = this.age - 1;
+    }
+    else {
+      alert('Age can not be less than zero')
+    }
+  }
+  setAgeZero() {
+    this.age = 0;
+  }
 }
